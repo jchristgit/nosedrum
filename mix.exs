6e700b3b6c53b30ca64d9c6c060da85a89cc4847
Johannes Christ <jc@jchri.st>
2019-01-31 23:52:00 +0100

    Do not start Nostrum in tests.

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Nosedrum.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      applications: [],
       extra_applications: [:logger]
     ]
   end
