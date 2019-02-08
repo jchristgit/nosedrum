a3d094625dc3d4e6fa28b22309cc64ed11c12f0b
Johannes Christ <jc@jchri.st>
2019-02-08 21:23:40 +0100

    Finalize `MessageCache` behaviour.

--- lib/nosedrum/message_cache.ex
+++ lib/nosedrum/message_cache.ex
@@ -1,6 +1,6 @@
 defmodule Nosedrum.MessageCache do
   @moduledoc """
-  A cache for recent messages on a guild.
+  A cache for recent messages sent on guilds.
 
   Usage of implementations of this module is useful when your bot needs
   to access recently sent messages without hogging the API. One use case
@@ -8,35 +8,51 @@ defmodule Nosedrum.MessageCache do
   after comparison of edited messages.
   """
 
-  alias Nostrum.Struct.{Guild, Message}
+  alias Nostrum.Struct.{Channel, Guild, Message, User}
 
-  @doc """
-  Retrieve a single message for the given `guild_id` with the specified `message_id` from the cache.
+  @typedoc """
+  A slimmed down message with the most relevant data.
 
-  If no message with the given ID is cached, `{:error, :not_found}` is returned.
+  You can fetch information about the author and the channel using
+  Nostrum's built in cache, specificaly `Nostrum.Cache.ChannelCache.get/1`
+  and `Nostrum.Cache.UserCache.get/1`.
   """
-  @callback get(guild_id :: Guild.id(), message_id :: Message.id()) ::
-              {:ok, Message.t()} | {:error, :not_found} | {:error, String.t()}
+  @type cache_message :: {Message.id(), Channel.id(), User.id(), Message.content()}
 
   @doc """
-  Retrieve up to `limit` messages for the given `guild_id` from the cache.
+  Retrieve a single message for the given `guild_id` with the specified `message_id`
+  from the cache process identified by `cache`.
+
+  If no message with the given ID is cached, `nil` is returned.
+  """
+  @callback get(guild_id :: Guild.id(), message_id :: Message.id(), cache :: reference()) ::
+              cache_message | nil | {:error, String.t()}
+
+  @doc """
+  Retrieve up to `limit` messages for the given `guild_id` from the cache
+  process identified by `cache`. If `limit` is `nil`, return all
+  messages for that guild from the cache.
 
   If the guild is not cached, an empty message list is returned.
   """
-  @callback recent_in_guild(guild_id :: Guild.id(), limit :: pos_integer) ::
-              {:ok, [Message.t()]} | {:error, String.t()}
+  @callback recent_in_guild(
+              guild_id :: Guild.id(),
+              limit :: pos_integer | nil,
+              cache :: reference()
+            ) ::
+              {:ok, [cache_message]} | {:error, String.t()}
 
   @doc """
-  Consume the given `message` in the cache.
+  Consume the given `message` in the cache process identified by `cache`.
 
   Whether this is done asynchronously or synchronously depends on the implementation.
   """
-  @callback consume(message :: Message.t()) :: :ok
+  @callback consume(message :: Message.t(), cache :: reference()) :: :ok
 
   @doc """
   Update the given `message` in the cache, if present.
 
   Whether this is done asynchronously or synchronously depends on the implementation.
   """
-  @callback update(message :: Message.t()) :: :ok
+  @callback update(message :: Message.t(), cache :: reference()) :: :ok
 end
